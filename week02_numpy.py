46e719b75bc1d9b696abd0d28d88badf26076961
voidgogo <2021038@daelim.ac.kr>
2023-09-07 16:15:35 +0900

    week02 v0.5

--- week02_numpy.py
+++ week02_numpy.py
@@ -2,10 +2,7 @@ import numpy as np
 import random
 
 n = int(input("input number : "))
-l = list()
-for i in range(n):
-    l.append(random.randint(1, 100))
-
+l = [random.randint(1, 100) for i in range(n)]
 v = np.array(l, dtype='int16')
 print(v)
 print(v.ndim, v.shape, v.data, v.dtype, v.strides)
