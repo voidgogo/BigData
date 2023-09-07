22c43690d1fb366ec3e968ff5b32151ef51f7d95
voidgogo <2021038@daelim.ac.kr>
2023-09-07 16:35:46 +0900

    week02 v0.7

--- week02_numpy.py
+++ week02_numpy.py
@@ -1,8 +1,31 @@
 import numpy as np
 import random
+import tkinter as tk  # Built in GUI
 
-n = int(input("input number : "))
-l = [random.randint(1, 100) for i in range(n)]
-v = np.array(l, dtype='int16')
-print(v)
-# print(v.ndim, v.shape, v.data, v.dtype, v.strides)
+def click_button():
+    n = int(en_number.get())
+    l = [random.randint(1, 100) for i in range(n)]
+    v = np.array(l, dtype='int16')
+    lbl_result.config(text=v)
+
+
+window = tk.Tk()
+window.title('numpy gui version v0.7')
+window.geometry('300x150')
+
+# create widget
+lbl_result = tk.Label(text="random numpy array")
+en_number = tk.Entry()
+btn_click = tk.Button(text="click me!", command=click_button)
+
+# widget layout
+lbl_result.pack()
+en_number.pack(fill='x')
+btn_click.pack(fill='x')
+
+window.mainloop()
+# n = int(input("input number : "))
+# l = [random.randint(1, 100) for i in range(n)]
+# v = np.array(l, dtype='int16')
+# print(v)
+
