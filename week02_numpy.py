bf14377e523592f0d3bb15c902d15083eef80910
voidgogo <2021038@daelim.ac.kr>
2023-09-07 16:13:14 +0900

    week02 v0.4

--- week02_numpy.py
+++ week02_numpy.py
@@ -1,8 +1,11 @@
 import numpy as np
+import random
 
-# v = np.array([1, 3, -9, 2])
-# v = np.array([1, 3, -9, 2], dtype='int64')
-v = np.array([
-    [1, 3, -9, 2],
-    [71, 13, -22, 7]])#, dtype='int64')
+n = int(input("input number : "))
+l = list()
+for i in range(n):
+    l.append(random.randint(1, 100))
+
+v = np.array(l, dtype='int16')
+print(v)
 print(v.ndim, v.shape, v.data, v.dtype, v.strides)
