fdc9727555c69a3459b752262c7cb0553cead3cb
voidgogo <2021038@daelim.ac.kr>
2023-09-07 14:52:32 +0900

    week02 v0.3

--- week02_numpy.py
+++ week02_numpy.py
@@ -1,4 +1,8 @@
 import numpy as np
 
-v = np.array([1, 3, -9, 2])
-print(v.ndim)
+# v = np.array([1, 3, -9, 2])
+# v = np.array([1, 3, -9, 2], dtype='int64')
+v = np.array([
+    [1, 3, -9, 2],
+    [71, 13, -22, 7]])#, dtype='int64')
+print(v.ndim, v.shape, v.data, v.dtype, v.strides)
