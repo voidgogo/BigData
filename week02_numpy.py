9ad8d90ac70bda73fb1e0c8e56913f68325af506
Daelim Kim <91718857+voidgogo@users.noreply.github.com>
2023-09-07 16:17:53 +0900

    week02 v0.6

--- week02_numpy.py
+++ week02_numpy.py
@@ -5,4 +5,4 @@ n = int(input("input number : "))
 l = [random.randint(1, 100) for i in range(n)]
 v = np.array(l, dtype='int16')
 print(v)
-print(v.ndim, v.shape, v.data, v.dtype, v.strides)
+# print(v.ndim, v.shape, v.data, v.dtype, v.strides)
