9faf2b103638f3d8a98b5a15b6230dbc9456ddfd
voidgogo <2021038@daelim.ac.kr>
2023-09-14 15:41:01 +0900

    week03 v1.5 2d array

--- week03_numpy.py
+++ week03_numpy.py
@@ -1,15 +1,25 @@
 import numpy as np
-import random
+# import random
 import tkinter as tk  # Built in GUI
 from tkinter import messagebox  # pop-up window
+
+
+def create_2d_array(row, col):
+    """
+    행과 열값을 입력 받아 2차원 넘파이 배열을 반환하는 함수
+    :param row: 행
+    :param col: 열
+    :return: 넘파이 2차원 배열
+    """
+    return np.random.randint(1, 101, size=(row, col))
 
 
 def click_button():
     try:
         r, c = map(int, en_row_column.get().split())  # spacebar
-
-        rows = [[random.randint(1, 100) for i in range(r)] for i in range(c)]
-        matrix = np.array(rows, dtype='int16')
+        # rows = [[random.randint(1, 100) for i in range(r)] for i in range(c)]
+        # matrix = np.array(create_2d_array(r, c), dtype='int16')
+        matrix = create_2d_array(r, c)
         lbl_result.config(text=matrix)
     except ValueError as err:
         lbl_result.config(text=f"입력 값이 없습니다.\n{err}")
@@ -17,7 +27,7 @@ def click_button():
 
 
 window = tk.Tk()
-window.title('numpy gui version v1.3')
+window.title('numpy gui version v1.5')
 window.geometry('300x150')
 
 # create widget
