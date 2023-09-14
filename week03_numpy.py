82e2d73f0a244028351d6347623ae471ff1776cb
voidgogo <2021038@daelim.ac.kr>
2023-09-14 15:25:34 +0900

    week03 v1.2 2d array

--- week03_numpy.py
+++ week03_numpy.py
@@ -9,9 +9,7 @@ def click_button():
         r = int(en_row.get())
         c = int(en_column.get())
 
-        rows = list()
-        for i in range(c):
-            rows.append([random.randint(1, 100) for i in range(r)])
+        rows = [[random.randint(1, 100) for i in range(r)] for i in range(c)]
         matrix = np.array(rows, dtype='int16')
         lbl_result.config(text=matrix)
     except ValueError as err:
@@ -20,7 +18,7 @@ def click_button():
 
 
 window = tk.Tk()
-window.title('numpy gui version v1.1')
+window.title('numpy gui version v1.2')
 window.geometry('300x150')
 
 # create widget
