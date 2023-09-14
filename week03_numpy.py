5816609830a2fa963d7971f8429b603738bb6f2e
voidgogo <2021038@daelim.ac.kr>
2023-09-14 14:41:06 +0900

    week03 v0.9 pop up

--- week03_numpy.py
+++ week03_numpy.py
@@ -1,6 +1,8 @@
 import numpy as np
 import random
 import tkinter as tk  # Built in GUI
+from tkinter import messagebox  # pop-up window
+
 
 def click_button():
     try:
@@ -9,11 +11,12 @@ def click_button():
         v = np.array(l, dtype='int16')
         lbl_result.config(text=v)
     except ValueError as err:
-        lbl_result.config(text=f"입력 값이 없습니다.\n{err}")
+        # lbl_result.config(text=f"입력 값이 없습니다.\n{err}")
+        messagebox.showerror('Error!', f"입력 값이 없습니다.\n{err}")
 
 
 window = tk.Tk()
-window.title('numpy gui version v0.7')
+window.title('numpy gui version v0.9')
 window.geometry('300x150')
 
 # create widget
