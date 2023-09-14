859f67469c82b292744f47c634aa4297d784613c
voidgogo <2021038@daelim.ac.kr>
2023-09-14 15:29:12 +0900

    week03 v1.3 2d array

--- week03_numpy.py
+++ week03_numpy.py
@@ -6,8 +6,7 @@ from tkinter import messagebox  # pop-up window
 
 def click_button():
     try:
-        r = int(en_row.get())
-        c = int(en_column.get())
+        r, c = map(int, en_row_column.get().split())  # spacebar
 
         rows = [[random.randint(1, 100) for i in range(r)] for i in range(c)]
         matrix = np.array(rows, dtype='int16')
@@ -18,19 +17,22 @@ def click_button():
 
 
 window = tk.Tk()
-window.title('numpy gui version v1.2')
+window.title('numpy gui version v1.3')
 window.geometry('300x150')
 
 # create widget
 lbl_result = tk.Label(text="random numpy 2d array")
-en_row = tk.Entry()
-en_column = tk.Entry()
+en_row_column = tk.Entry()
+#en_column = tk.Entry()
 btn_click = tk.Button(text="click me!", command=click_button)
 
 # widget layout
-lbl_result.grid(row=0, column=0, columnspan=2)
-en_row.grid(row=1, column=0)
-en_column.grid(row=1, column=1)
-btn_click.grid(row=2, column=0, columnspan=2, sticky=tk.EW)
+# lbl_result.grid(row=0, column=0, columnspan=2)
+# en_row.grid(row=1, column=0)
+# en_column.grid(row=1, column=1)
+# btn_click.grid(row=2, column=0, columnspan=2, sticky=tk.EW)
+lbl_result.pack()
+en_row_column.pack(fill='x')
+btn_click.pack(fill='x')
 
 window.mainloop()
