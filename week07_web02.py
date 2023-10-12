e837005b2ec44f6f3ab8eed069f3c24b685cb6b5
voidgogo <2021038@daelim.ac.kr>
2023-10-12 16:25:00 +0900

    week07 v3.7 web

--- week07_web02.py
+++ week07_web02.py
@@ -7,8 +7,10 @@ soup = BeautifulSoup(urls, 'html.parser')
 
 cities = soup.find_all("city")
 data = soup.find_all("data")
+dates = soup.find("tmef")
 
+print(dates.string)
 for i in range(len(cities)):
-    print(f'{cities[i].string}의 중기 예보 첫 번째 날씨는 {data[i*13].find("wf").string}입니다.')
+    print(f'{cities[i].string}의 날씨는 {data[i*13].find("wf").string}입니다.')
 
 print(len(cities), len(data))
