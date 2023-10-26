332720ed3579acae1c74b3920c5b654ca2f266e3
voidgogo <2021038@daelim.ac.kr>
2023-10-26 15:16:29 +0900

    week09 v3.9 numpy & pandas

--- week09_numpy_review.py
+++ week09_numpy_review.py
@@ -1,29 +1,13 @@
 import numpy as np
 
+def info(x):
+    print(f"배열의 차원(rank)은 {x.ndim}차원 입니다")
+    print(f"배열의 shape는 {x.shape}입니다")
+    print(f"배열 원소의 개수는 {x.size}개 입니다")
+
 np_1d = np.arange(1, 20, 2)
-#np_1d = np.arange(1.0, 10.0, 2.0)
-np_1d = np_1d.reshape(5, 2)
-print(np_1d, type(np_1d))
-print(np_1d, type(np_1d[0]))
-print(np_1d.ndim)  # n-dimension, rank
-print(np_1d.size)  # size
-print(np_1d.shape)  # shape
-
-
-# np_1d = np.array([2, 4, 6, 8])
-# np_2d = np.array([[2, 4], [6, 8]])
-# print(np_1d.ndim, np_2d.ndim)  # n-dimension, rank
-# print(np_1d.size, np_2d.size)  # size
-# print(np_1d.shape, np_2d.shape)  # shape
-
-
-# any_list = [91, "university", 3.91, (5, 7)]
-# converted_any_list = ["91", "university", "3.91", "(5, 7)"]
-#one_type_np_list = np.array(converted_any_list)
-#one_type_np_list = np.array(any_list)  # ValueError
-
-# for i in any_list:
-#     print(i)
-#
-# for j in one_type_np_list:
-#     print(j)
+print(np_1d)
+info(np_1d)
+np_1d = np_1d.reshape(1, 2, 5)
+print(np_1d)
+info(np_1d)
