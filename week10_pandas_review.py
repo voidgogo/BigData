84ace6e12b1c75f2fb23b0f4020b32c66da54f40
voidgogo <2021038@daelim.ac.kr>
2023-11-02 16:14:12 +0900

    week10 v4.8 numpy & pandas

--- week10_pandas_review.py
+++ week10_pandas_review.py
@@ -1,23 +1,14 @@
 import numpy as np
-a1 = np.arange(1, 6)
-a2 = np.array([6, 7, 8, 9, 10])
-a3 = np.array([
-    [3, 2, 1],
-    [-11, 0, 9]
+a = np.arange(1, 7).reshape(2, 3)
+b = np.array([
+    [5, 20],
+    [11, -1],
+    [9, 3]
 ])
-print(a1 + a2)
-print(a1 - a2)
-print(a1 * a2)
-print(a1 / a2)
-print(np.dot(a1, a2))
-# a3 = a1 + a2
-# print(a3)
-print(np.sum(a1) + np.sum(a2))
-print(np.prod(a1), np.prod(a2))
-# by axis
-print(a3.sum(axis=0))
-print(a3.sum(axis=1))
-print(a3.prod(axis=0))
-print(a3.prod(axis=1))
-print(a3.mean())  # 산술평균
-print(a3.std())  # 표준편차
+print(a)
+print(b)
+print()
+print(a.dot(b))
+print(np.dot(a, b))
+print(b.dot(a))
+print(np.dot(b, a))
