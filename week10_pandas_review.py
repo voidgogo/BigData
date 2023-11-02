be15b310a7d773a4e8b54dbe91a19ffe5fb08e33
voidgogo <2021038@daelim.ac.kr>
2023-11-02 15:37:18 +0900

    week10 v4.6 numpy & pandas

--- week10_pandas_review.py
+++ week10_pandas_review.py
@@ -2,10 +2,18 @@ import numpy as np
 a1 = np.ones((3, 3))
 a1[:, 2] = 2
 print(a1)
-a2 = np.array([
-    [1, 0, 0],
-    [2, 0, 0],
-    [1, 0, 0],
-])
+a2 = np.full((3, 3), 5)
 print(a2)
-print(np.matmul(a1, a2))  # 행렬 곱
+a3 = np.eye(3)  # 단위 행렬
+print(a3)
+a4 = a3[:2, 1:]
+print(a4)
+#a5 = a3[2]  # (3,)
+a5 = a3[2, :]  # (3,)
+#a5 = a3[2:, :]  # (1, 3)
+print(a5)
+#a6 = a3[1, :2]  # (2,)
+a6 = a3[1:2, :2]  # (1, 2)
+print(a6)
+a7 = a3[:, :2]  # (3, 2)
+print(a7)
