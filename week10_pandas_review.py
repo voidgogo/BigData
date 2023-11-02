d9a2b5b326bb4dbaf4de64f2a512347ad02a4e18
voidgogo <2021038@daelim.ac.kr>
2023-11-02 15:21:51 +0900

    week10 v4.5 numpy & pandas

--- week10_pandas_review.py
+++ week10_pandas_review.py
@@ -1,12 +1,11 @@
 import numpy as np
-a1 = np.array([
-    [1, 1, 2],
-    [1, 1, 2],
-    [1, 1, 2]
-])
+a1 = np.ones((3, 3))
+a1[:, 2] = 2
+print(a1)
 a2 = np.array([
     [1, 0, 0],
     [2, 0, 0],
     [1, 0, 0],
 ])
+print(a2)
 print(np.matmul(a1, a2))  # 행렬 곱
