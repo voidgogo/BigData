a041d146832fbe25e9c078e029fcd10504b60e84
voidgogo <2021038@daelim.ac.kr>
2023-11-02 15:48:40 +0900

    week10 v4.7 numpy & pandas

--- week10_pandas_review.py
+++ week10_pandas_review.py
@@ -1,19 +1,23 @@
 import numpy as np
-a1 = np.ones((3, 3))
-a1[:, 2] = 2
-print(a1)
-a2 = np.full((3, 3), 5)
-print(a2)
-a3 = np.eye(3)  # 단위 행렬
-print(a3)
-a4 = a3[:2, 1:]
-print(a4)
-#a5 = a3[2]  # (3,)
-a5 = a3[2, :]  # (3,)
-#a5 = a3[2:, :]  # (1, 3)
-print(a5)
-#a6 = a3[1, :2]  # (2,)
-a6 = a3[1:2, :2]  # (1, 2)
-print(a6)
-a7 = a3[:, :2]  # (3, 2)
-print(a7)
+a1 = np.arange(1, 6)
+a2 = np.array([6, 7, 8, 9, 10])
+a3 = np.array([
+    [3, 2, 1],
+    [-11, 0, 9]
+])
+print(a1 + a2)
+print(a1 - a2)
+print(a1 * a2)
+print(a1 / a2)
+print(np.dot(a1, a2))
+# a3 = a1 + a2
+# print(a3)
+print(np.sum(a1) + np.sum(a2))
+print(np.prod(a1), np.prod(a2))
+# by axis
+print(a3.sum(axis=0))
+print(a3.sum(axis=1))
+print(a3.prod(axis=0))
+print(a3.prod(axis=1))
+print(a3.mean())  # 산술평균
+print(a3.std())  # 표준편차
