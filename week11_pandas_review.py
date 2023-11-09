fd1152e1e5ca05297cfdeda20e46b0fc94d9ea9d
voidgogo <2021038@daelim.ac.kr>
2023-11-09 15:22:10 +0900

    week11 v5.5 numpy & pandas

--- week11_pandas_review.py
+++ week11_pandas_review.py
@@ -1,33 +1,25 @@
 import pandas as pd
 
-df1 = pd.DataFrame(
-    {
-        "KOR": [99, 91, 100],
-        "ENG": [89, 98, 97],
-        "MAT": [100, 90, 85],
-    }, index=[1, 2, 3]
-)
-print(df1)
-print(df1.sort_values('MAT'))
-df1 = df1.drop(columns=['ENG'])
-print(df1)
-
-df2 = pd.DataFrame(
+df = pd.DataFrame(
     [[99, 89, 100],
         [91, 98, 90],
-        [100, 97, 85]],
-    index=[1, 2, 3],
+        [100, 97, 85],
+        [83, 100, 94]],
+    index=[1, 2, 3, 4],
     columns=['KOR', 'ENG', 'MAT']
 )
-print(df2)
-df2 = (pd.melt(df2)
+print(df)
+# df_copy = df.iloc[:, [0, 2]]
+# df_copy = df.loc[:, ['KOR', 'MAT']]
+df_copy = df.loc[:, 'KOR':'MAT':2]
+print(df_copy)
+
+df = (pd.melt(df)
        .rename(columns={
         'variable': 'subject',
         'value': 'score'})
        .query('score >= 90')
        .sort_values('score', ascending=False)
        )
-
-print(df2)
-
-
+print(df)
+print(df.iloc[:, [1]])
