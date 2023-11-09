8e9aa2f46bef34bbb78913ee6de126ffab8645f6
voidgogo <2021038@daelim.ac.kr>
2023-11-09 14:33:03 +0900

    week11 v5.3 numpy & pandas

--- week11_pandas_review.py
+++ week11_pandas_review.py
@@ -7,7 +7,8 @@ df1 = pd.DataFrame(
         "MAT": [100, 90, 85],
     }, index=[1, 2, 3]
 )
-# print(df1)
+print(df1)
+print(df1.sort_values('MAT'))
 
 df2 = pd.DataFrame(
     [[99, 89, 100],
@@ -22,6 +23,7 @@ df2 = (pd.melt(df2)
         'variable': 'subject',
         'value': 'score'})
        .query('score >= 90')
+       .sort_values('score', ascending=False)
        )
 
 print(df2)
