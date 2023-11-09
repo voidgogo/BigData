e3c987b602baddd53c932095cc801b4cc8b26bc0
voidgogo <2021038@daelim.ac.kr>
2023-11-09 16:05:07 +0900

    week11 v5.8 numpy & pandas

--- week11_pandas_review.py
+++ week11_pandas_review.py
@@ -1,10 +1,10 @@
 import pandas as pd
 
 df = pd.DataFrame(
-    [[99, 89, 100],
+    [[99, 89, 81],
         [91, 98, 90],
-        [100, 97, 85],
-        [83, 100, 94]],
+        [95, 97, 85],
+        [83, 96, 94]],
     index=[1, 2, 3, 4],
     columns=['KOR', 'ENG', 'MAT']
 )
@@ -16,7 +16,7 @@ print(df.max())
 print(df)
 # 국어 성적과 영어 성적이 둘 다 95점 이상인 행을 추출
 print(df.query('KOR>=95 and ENG>=95'))
-# 1번 학생의 수학 성적(100) 출력
+# 1번 학생의 수학 성적(81) 출력
 print(df.at[1, 'MAT'])
 #print(df.iat[1, 3])  # error, 0행 0열 부터 시작한다. index기반 (label X)
 print(df.iat[0, 2])
@@ -24,3 +24,11 @@ print(df.iat[0, 2])
 # 조건은 국어 성적이 95점 이상인 경우
 df_copy = df.loc[df['KOR']>=95, ['KOR', 'MAT']]
 print(df_copy)
+
+
+def scale_score(n):
+    return n + 1
+
+
+df = df.apply(scale_score)
+print(df)
