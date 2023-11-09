d6e3790aede75bb75e2b3da773f002ec1c3baea5
voidgogo <2021038@daelim.ac.kr>
2023-11-09 16:30:44 +0900

    week11 v6.0 numpy & pandas

--- week11_pandas_review.py
+++ week11_pandas_review.py
@@ -1,22 +1,28 @@
-import pandas as pd
-
-df = pd.DataFrame(
-    [[99, 89, 81],
+scores = [[99, 89, 81],
         [91, 98, 90],
         [95, 97, 85],
-        [83, 96, 94]],
-    index=[1, 2, 3, 4],
-    columns=['KOR', 'ENG', 'MAT']
-)
+        [83, 96, 94]]
 
-# def scale_score(n):
-#     return n + 1
-#
-#
-# print(df)
-# df = df.apply(scale_score)
-# print(df)
+index = [1, 2, 3, 4]
+columns = ['KOR', 'ENG', 'MAT']
 
-print(df)
-df = df.apply(lambda n: n + 1)
-print(df)
+for c in columns:
+    print(f'{c}', end=' ')
+print()
+for row in range(4):
+    print(index[row], end=' ')
+    for col in range(3):
+        print(scores[row][col], end=' ')
+        scores[row][col] = scores[row][col]+1  # update
+    print()
+
+print()
+
+for c in columns:
+    print(f'{c}', end=' ')
+print()
+for row in range(4):
+    print(index[row], end=' ')
+    for col in range(3):
+        print(scores[row][col], end=' ')
+    print()
