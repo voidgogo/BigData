3de542e171d6934de0036d20f5b4f7953b4511a8
voidgogo <2021038@daelim.ac.kr>
2023-11-09 15:33:23 +0900

    week11 v5.6 numpy & pandas

--- week11_pandas_review.py
+++ week11_pandas_review.py
@@ -9,17 +9,11 @@ df = pd.DataFrame(
     columns=['KOR', 'ENG', 'MAT']
 )
 print(df)
-# df_copy = df.iloc[:, [0, 2]]
-# df_copy = df.loc[:, ['KOR', 'MAT']]
-df_copy = df.loc[:, 'KOR':'MAT':2]
+# 1번 학생의 수학 성적(100) 출력
+print(df.at[1, 'MAT'])
+#print(df.iat[1, 3])
+print(df.iat[0, 2])
+# 국어, 수학 칼럼을 추출
+# 조건은 국어 성적이 95점 이상인 경우
+df_copy = df.loc[df['KOR']>=95, ['KOR', 'MAT']]
 print(df_copy)
-
-df = (pd.melt(df)
-       .rename(columns={
-        'variable': 'subject',
-        'value': 'score'})
-       .query('score >= 90')
-       .sort_values('score', ascending=False)
-       )
-print(df)
-print(df.iloc[:, [1]])
