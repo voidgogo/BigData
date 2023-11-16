60d493573dd66a96ca6d0db71eb231b06c8afc88
voidgogo <2021038@daelim.ac.kr>
2023-11-16 15:11:26 +0900

    week12 v6.3 web

--- week12_web03.py
+++ week12_web03.py
@@ -6,20 +6,16 @@ html = """
 <title>스크레이핑 실습</title>
 </head>
 <body>
-<h1>대림대학교</h1>
+<h1 id="univ">대림대학교</h1>
 <p>웹스크레이핑</p>
-<p>파이썬, 판다스, 넘파이, 맷플롯립, GUI...</p>
+<p id="contents">파이썬, 판다스, 넘파이, 맷플롯립, GUI...</p>
 </body>
 </html>
 """
 soup = BeautifulSoup(html, 'html.parser')
-t = soup.html.head.title
-print(t)
-print(t.string)
-h1 = soup.html.body.h1.string
-print(h1)
-p1 = soup.html.body.p.string
-#p2 = soup.html.body.p.string
-p2 = p1.next_sibling.next_sibling
-print(p1)
-print(p2)
+university = soup.find(id='univ')
+contents = soup.find(id='contents')
+
+print(university)
+print(contents.string)
+print(contents)
