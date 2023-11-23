199caae83b0489e82d0e52ad18e94f0fa313e157
voidgogo <2021038@daelim.ac.kr>
2023-11-23 16:32:36 +0900

    week13 v6.8

--- week13_review.py
+++ week13_review.py
@@ -3,9 +3,15 @@ import pandas as pd
 
 titanic = sns.load_dataset('titanic')
 
-print(titanic.isnull())
+print(titanic.isnull().sum())
+print(titanic['age'].fillna(titanic['age'].median(), inplace=True))
+print(titanic.isnull().sum())
+# titanic['deck'].fillna('Unknown', inplace=True)  # 범주형 카테고리에 Unknown이 없음
+titanic['deck'] = titanic['deck'].cat.add_categories('Unknown')  # 카테고리 추가
+titanic['deck'].fillna('Unknown', inplace=True)
 
 # print(type(titanic))
 # print(titanic.describe())
 # print(titanic.info())
-# print(titanic)
+# print(titanic.tail())
+print(titanic['deck'].tail(10))
