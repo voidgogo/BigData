2be3af435c9c2bb698116bf54c1879766996a9da
voidgogo <2021038@daelim.ac.kr>
2023-11-23 15:21:54 +0900

    week13 v6.6

---
+++ week13_review.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+a1 = np.ones(5)
+a2 = np.array([1, 1, 1, 1, 1])
+a3 = np.arange(5) * 0 + 1
+a4 = np.zeros(5) + 1
+a5 = np.linspace(1, 1, 5)
+a6 = np.full(5, 1)
+print(a1, a2, a3, a4, a5, a6)
