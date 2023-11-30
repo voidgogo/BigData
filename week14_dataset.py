1e77e80bf91333b7f1f381b4b4ebbe47d32c5137
voidgogo <2021038@daelim.ac.kr>
2023-11-30 15:02:40 +0900

    week14 v7.1

--- week14_dataset.py
+++ week14_dataset.py
@@ -8,9 +8,12 @@ import seaborn as sns
 titanic = sns.load_dataset('titanic')
 # print(titanic.info())
 # print(titanic.head())
+male_survived = titanic[(titanic['survived'] == 1) & (titanic['sex'] == 'male')]['survived'].count()
+female_survived = titanic[(titanic['survived'] == 1) & (titanic['sex'] == 'female')]['survived'].count()
+male_count = titanic[(titanic['sex'] == 'male')]['sex'].count()
+female_count = titanic[(titanic['sex'] == 'female')]['sex'].count()
+print(male_survived, male_count, female_survived, female_count)
+print(male_survived/male_count, female_survived/female_count)
 
-# survived_human = titanic[titanic["survived"] == 1]["survived"].count()
-# dead_human = titanic[titanic["survived"] == 0]["survived"].count()
-# print(f"생존자 수 : {survived_human}명")
-# print(f"사망자 수 : {dead_human}명")
+
 
