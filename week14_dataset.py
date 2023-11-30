8d3f36afea93f02b5601da1cea6f1d8138dd553f
voidgogo <2021038@daelim.ac.kr>
2023-11-30 15:25:05 +0900

    week14 v7.3

--- week14_dataset.py
+++ week14_dataset.py
@@ -2,16 +2,16 @@
 1) 생존자와 사망자 수를 구하시오.
 2) 남성과 여성의 생존률을 구하시오.
 3) 객실 등급별 생존자 수를 구하시오.
+4) 나이대별 생존자 수를 구하시오.
 
 """
 import seaborn as sns
+import pandas as pd
 
 titanic = sns.load_dataset('titanic')
 # print(titanic.info())
 # print(titanic.head(10))
-pclass_survived = titanic.groupby('pclass')['survived'].sum()
-# feat. jm lee
-# pclass_survived = titanic[titanic['survived'] == 1].groupby(['pclass']).size()
-print(pclass_survived)
 
-
+# [0, 10, 20, 30, 40, 50, 60 , 70, 80]
+age_survived = titanic.groupby(pd.cut(titanic['age'], bins=range(0, 81, 10)))['survived'].sum()
+print(age_survived)
